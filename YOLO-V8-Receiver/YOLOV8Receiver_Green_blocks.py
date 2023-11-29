97fbb2ddfd4bdb89e2742843627ef393fea8c0a3
Reid Graves <110854517+gravesreid@users.noreply.github.com>
2023-11-29 15:27:43 -0500

    Add files via upload

---
+++ YOLO-V8-Receiver/YOLOV8Receiver_Green_blocks.py
@@ -0,0 +1,67 @@
+import cv2
+import socket
+import struct
+import numpy as np
+from ultralytics import YOLO
+
+
+# Load YOLO
+model = YOLO('green_block2.pt')
+model.conf = 0.25
+
+#get output from YOLO V8
+def detect_objects(frame):
+    results = model([frame], stream=True)
+    return results
+
+# Create a socket for receiving data
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host_ip = '192.168.1.11'  # IP Address
+port = 12345  # Must be the same as in the sender
+
+socket_address = (host_ip, port)
+
+# Bind to the address and listen
+server_socket.bind(socket_address)
+server_socket.listen(5)
+print("Listening at:", socket_address)
+
+# Accept a connection
+client_socket, addr = server_socket.accept()
+print('Connection from:', addr)
+
+data = b""
+payload_size = struct.calcsize(">L")
+
+while True:
+    # Retrieve message size
+    while len(data) < payload_size:
+        data += client_socket.recv(4096)
+
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack(">L", packed_msg_size)[0]
+
+    # Retrieve all data based on message size
+    while len(data) < msg_size:
+        data += client_socket.recv(4096)
+
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+
+    # Decompress frame
+    frame = cv2.imdecode(np.frombuffer(frame_data, dtype=np.uint8), cv2.IMREAD_COLOR)
+
+    if frame is not None:
+        results = model(frame)
+
+        annotated_frame = results[0].plot()
+
+        cv2.imshow("YOLOv8 Inference", annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+client_socket.close()
+cv2.destroyAllWindows()
+
