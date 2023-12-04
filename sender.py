615055e69e9c9a7e0bee0872c8645041d99bbb18
Reid Graves <110854517+gravesreid@users.noreply.github.com>
2023-12-04 13:04:43 -0500

    Add files via upload

--- sender.py
+++ sender.py
@@ -1,36 +1,34 @@
 import cv2
 import socket
 import struct
+import time
 
-video_path = 'example.mp4'
-cap = cv2.VideoCapture(video_path)
+cap = cv2.VideoCapture(0)
 
-# Create a socket for sending data
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host_ip = '192.168.1.3'  # Replace with receiver's IP address
-port = 12345  # Port number
+host_ip = '192.168.1.11'
+port = 12345
 
-# Connect to the receiver
-client_socket.connect((host_ip, port))
+client_socket.connect((host_ip,port))
 
+n = 10
+frame_count = 0
 while cap.isOpened():
     ret, frame = cap.read()
-    if not ret:
-        break
+    frame = cv2.resize(frame, (640,480))
+    frame_count += 1
+    if not ret or frame_count % n != 0:
+        continue
+    currentTime = time.time()
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 80]
+    _, buffer = cv2.imencode('.jpg', frame, encode_param)
+    print("Time taken: ",time.time() - currentTime)
 
-    # Compress the frame using JPEG
-    ret, buffer = cv2.imencode('.jpg', frame)
-
-    # Convert to bytes and get the size
     data = buffer.tobytes()
     size = len(data)
+    print("Size: ", size)
 
-    # Send the size of the frame first
-    client_socket.sendall(struct.pack(">L", size))
-
-    # Then send the actual frame
-    client_socket.sendall(data)
+    client_socket.sendall(struct.pack(">L", size) + data)
 
 cap.release()
 client_socket.close()
-
